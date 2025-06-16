4c6a3c9046ba8e24b15770a3df39419647233ea3
PiotrFlo <15006@eans-nt.edu.pl>
2025-06-16 23:21:40 +0200

    finish oop version

--- ticket.py
+++ ticket.py
@@ -1,7 +1,9 @@
 class Ticket:
-    def __init__(self, name, price):
+    def __init__(self, name, price, category, group):
         self.name = name
         self.price = price
+        self.category = category
+        self.group = group
 
     def __str__(self):
-        return f"{self.name}\t{self.price} zł"
+        return f"Bilet {self.category}\t{self.group}\t{self.name}\t{self.price} zł"
